c4e870cd025f43127e42f40ce1809fd2f9ebf50c
norm-ideal <idehara.n@gmail.com>
2021-04-26 00:32:08 +0900

    Main Game Loop

---
+++ Bardventure/Assets/DevelopmentFolders/DevRythmGameLoop/RhythmGameLoop.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhythmGameLoop : MonoBehaviour
+{
+    public enum GAME_STATE { IN_PLAY };
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
