005b085eb726a47787e5917359c19acb7dd501a1
Nicolas <nicolasjatob@orange.fr>
2021-06-09 11:32:06 +0200

    Corrected note classes

--- Bardventure/Assets/Nicolas/NoteListGenerator.cs
+++ Bardventure/Assets/Nicolas/NoteListGenerator.cs
@@ -5,14 +5,14 @@ using Melanchall.DryWetMidi.Core;
 using Melanchall.DryWetMidi.Interaction;
 
 //Swap to using UnityEngine.Random because 20% 40% faster ?
-enum Gestures { Thumbs_up, Victory_sign, Closed, Open };
+//enum Gestures { Thumbs_up, Victory_sign, Closed, Open };
 public static class NoteListGenerator
 {
     public static void GenerateList(RhythmCore rhythmCore)
     {
         Debug.Log("Static list generator properly called");
-        Gestures gesture = new Gestures();
-        var values = System.Enum.GetValues(typeof(Gestures));
+        //Gestures gesture = new Gestures();
+        //var values = System.Enum.GetValues(typeof(Gestures));
         System.Random myrand = new System.Random();
         int track = 0;
         MidiFile mymidi = MidiFile.Read("Assets/Nicolas/Audio/" + rhythmCore.getNameOfSong() + ".mid");
@@ -24,14 +24,14 @@ public static class NoteListGenerator
 
         foreach (Note note in listNote)
         {
-            gesture = (Gestures)values.GetValue(myrand.Next(values.Length));
+            //gesture = (Gestures)values.GetValue(myrand.Next(values.Length));
             track = Random.Range(0, 2);
             Debug.Log("Note channel :" + note.Channel);
 
             if(note.Channel == 1)
             {
                 if (note.LengthAs<MetricTimeSpan>(tempomap).TotalMicroseconds / 1000000f > 1f)
-                    rhythmCore.addToNoteList(new SpecialNote(note.LengthAs<MetricTimeSpan>(tempomap).TotalMicroseconds / 1000000f, 0, (int)gesture, 100, note.TimeAs<MetricTimeSpan>(tempomap).TotalMicroseconds / 1000000f, 1f, track));
+                    rhythmCore.addToNoteList(new SpecialNote(note.LengthAs<MetricTimeSpan>(tempomap).TotalMicroseconds / 1000000f, 0, 100, note.TimeAs<MetricTimeSpan>(tempomap).TotalMicroseconds / 1000000f, 1f, track));
                 else
                     rhythmCore.addToNoteList(new SimpleNote(0, 100, note.TimeAs<MetricTimeSpan>(tempomap).TotalMicroseconds / 1000000f, 1, track));
             }
